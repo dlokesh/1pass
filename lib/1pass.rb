fb4763e3eb38f4444218a6e27f0f796ec4d0e5e0
Lokeshwaran <dlokesh@gmail.com>
2013-08-17 22:03:09 +0530

    Display error message if key or password is invalid

--- lib/1pass.rb
+++ lib/1pass.rb
@@ -2,6 +2,9 @@ require "1pass/version"
 require "keychain"
 
 class AgileKeychain
+	INVALID_PASSWORD = "Maybe you should try asking a human?"
+	INVALID_KEY = "Invalid key. Keys are case-sensitive."
+
 	def initialize(path=nil)
 		path = path || "#{ENV["HOME"]}/Library/Application Support/1Password/1Password.agilekeychain"
 		@keychain = Keychain.new(path)
@@ -12,9 +15,15 @@ class AgileKeychain
 	end
 
 	def load(master_password, key_name, field_name=nil) 
-  		@keychain.unlock(master_password)
+  		inform_and_exit(INVALID_PASSWORD) unless @keychain.unlock(master_password)
   		key = @keychain.get(key_name)
-  		return unless key
+  		inform_and_exit(INVALID_KEY) unless key
   		puts field_name ? key.find(field_name) : key.fields
+	end
+
+	private
+	def inform_and_exit(message)
+		puts message
+		exit 1
 	end
 end
