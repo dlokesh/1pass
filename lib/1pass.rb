885ad94dc161af125a749a7bfeff18e7dec6f91e
Jonathan Smith <smithj@squareup.com>
2014-06-25 14:52:17 -0700

    raise exceptions instead of calling exit in a method

--- lib/1pass.rb
+++ lib/1pass.rb
@@ -1,6 +1,8 @@
 require "1pass/version"
 require "keychain"
 
+class AgileKeychainException < Exception ; end
+
 class AgileKeychain
 	INVALID_PASSWORD = "Maybe you should try asking a human?"
 	INVALID_KEY = "Invalid key. Keys are case-sensitive."
@@ -14,16 +16,10 @@ class AgileKeychain
 		@keychain.content.items.map {|i| puts i.name}
 	end
 
-	def load(master_password, key_name, field_name=nil) 
-  		inform_and_exit(INVALID_PASSWORD) unless @keychain.unlock(master_password)
-  		key = @keychain.get(key_name)
-  		inform_and_exit(INVALID_KEY) unless key
-  		puts field_name ? key.find(field_name) : key.fields
-	end
-
-	private
-	def inform_and_exit(message)
-		puts message
-		exit 1
+	def load(master_password, key_name, field_name=nil)
+		raise AgileKeychainException, INVALID_PASSWORD unless @keychain.unlock(master_password)
+		key = @keychain.get(key_name)
+		raise AgileKeychainException, INVALID_KEY unless key
+		puts field_name ? key.find(field_name) : key.fields
 	end
 end
