39d8292ed7ef61da14293c7afc80f133c7c9ba53
Jonathan Smith <smithj@squareup.com>
2014-06-25 16:08:37 -0700

    allow key unlocks to happen in AgileKeychain.initialize

--- lib/1pass.rb
+++ lib/1pass.rb
@@ -7,9 +7,11 @@ class AgileKeychain
 	INVALID_PASSWORD = "Invalid Password. Maybe you should try asking a human?"
 	INVALID_KEY = "Invalid key. Keys are case-sensitive."
 
-	def initialize(path=nil)
+	def initialize(path=nil, master_password=nil)
+		@unlocked = false
 		path = path || "#{ENV["HOME"]}/Library/Application Support/1Password/1Password.agilekeychain"
 		@keychain = Keychain.new(path)
+		unlock(master_password) if master_password
 	end
 
 	def list
@@ -17,9 +19,21 @@ class AgileKeychain
 	end
 
 	def load(master_password, key_name, field_name=nil)
-		raise AgileKeychainException, INVALID_PASSWORD unless @keychain.unlock(master_password)
+		# this is for api compatability
+		# if already unlocked, will be a noop. if not, we need to unlock it here!
+		unlock(master_password)
+
 		key = @keychain.get(key_name)
 		raise AgileKeychainException, INVALID_KEY unless key
 		puts field_name ? key.find(field_name) : key.fields
+	end
+
+	private
+	def unlock(master_password)
+		return true if @unlocked
+		return false if !master_password
+		raise AgileKeychainException, INVALID_PASSWORD unless @keychain.unlock(master_password)
+		@unlocked = true
+		return true
 	end
 end
